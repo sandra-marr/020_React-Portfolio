96c0cc8df18d23287f75fc9a2a72b275fb9028ff
SPeats <srpeters44@gmail.com>
2021-09-22 18:02:58 -0700

    updated styling for work page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,22 +11,15 @@ export function SkillCard(props) {
 }
 
 export function WorkCard(props) {
-
-    const style = {
-        background: 'transparent',
-        color: 'transparent'
-    };
-
-
     
     return (
         <div className="card w-100 p-2 mb-3">
-            <div className="row g-0">
-                <div class="col-md-8">
+            <div className="row g-0 workbackground">
+                <div className="col-md-8 d-flex align-content-center">
                     <img src={props.src} className="card-img mx-auto col" alt={props.name}/>
                 </div>
-                <div class="col-md-4">
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                <div className="col-md-4">
+                    <div className="card-body d-flex flex-column justify-content-center align-items-center">
                         <h5 className="card-title">{props.name}</h5>
                         <a href={props.deployed} target="_blank" rel="noreferrer">Deployed Application</a>
                         <a href={props.github} target="_blank" rel="noreferrer">GitHub Repository</a>
