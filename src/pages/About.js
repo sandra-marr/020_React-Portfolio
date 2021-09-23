a25f14bf8d08bf0cddd97a1e72b6b1e8f070eb7b
SPeats <srpeters44@gmail.com>
2021-09-22 20:58:49 -0700

    updated resume

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function AboutPage() {
         <Container fluid className="d-flex flex-column justify-content-center">
             <h2 className="accent pb-2 text-center">About Me</h2>
             <img id="profile" className="mb-3" src={ profile } alt="Sandy Marr Profile" />
-            <p>My name is Sandra Marr and I am an aspiring web developer, with a focus on front-end development. Since March of 2021, I discovered a passion in coding and have decided to make a career shift into web development. In early September, I graduated from the University of Washington's Full-Stack Coding Bootcamp Program. While there is still so much to learn, I believe that I have the base knowledge and skills needed to succeed in this industry. To date my professional experience has focused on Project Management, Structural Design Engineering, and Test Engineering, with some volunteer work peppered in as a treasurer for a non-profit.
+            <p>My name is Sandra Marr and I am an aspiring web developer, with a focus on front-end development. Since March of 2021, I discovered a passion in coding and have decided to make a career shift into web development. In early September, I graduated from the University of Washington's Full-Stack Coding Bootcamp Program. While there is still so much to learn, I believe that I have the base knowledge and skills needed to succeed in this industry. To date my professional experience has focused on Project Management, Structural Design Engineering, and Test Engineering, with some volunteer work peppered in as a treasurer and accountant for a non-profit.
             </p>
             <p>
             Starting in March, I was just learning about HTML and CSS. Through out the program there have been many challenges
